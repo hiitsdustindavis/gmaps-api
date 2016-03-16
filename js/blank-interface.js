9a6457a2223c0a71e1370efefbe9197a8bfa0586
Alex Fallenstedt and Dustin Davis <student@epicodus.com>
2016-03-16 11:30:55 -0700

    styled page

--- js/blank-interface.js
+++ js/blank-interface.js
@@ -1,9 +1,16 @@
 // var currentMap = require('./../js/map.js');
 var map;
 function initMap() {
+  var myLatLng = {lat: 20.363, lng: 131.044};
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 10
+  });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
   });
 }
 $(document).ready(function() {
