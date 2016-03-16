15619883c5d72302d85de51fd18a9e9026c4d810
Alex Fallenstedt and Dustin Davis <student@epicodus.com>
2016-03-16 12:23:59 -0700

    fix issues

--- js/blank-interface.js
+++ js/blank-interface.js
@@ -1,19 +1,13 @@
-// var currentMap = require('./../js/map.js');
+var currentLocation = require('./../js/currentLocation.js');
+
 var map;
 function initMap() {
-  var myLatLng = {lat: 20.363, lng: 131.044};
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 10
   });
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
 }
 $(document).ready(function() {
   google.maps.event.addDomListener(window, 'load', initMap);
-
+  $('#locateUser').click(currentLocation);
 });
