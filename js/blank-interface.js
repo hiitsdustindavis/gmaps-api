c35a317e7f65d413794832c872e9ddebff1c7085
Alex Fallenstedt and Dustin Davis <student@epicodus.com>
2016-03-16 16:16:41 -0700

    hey this is alexs and dustins project that Im commiting on and it doesnt work

--- js/blank-interface.js
+++ js/blank-interface.js
@@ -1,5 +1,5 @@
 var currentLocation = require('./../js/currentLocation.js');
-
+var routeFinder = require('./../js/routeFinder.js');
 
 $(document).ready(function() {
   google.maps.event.addDomListener(window, 'load', currentLocation.initMap);
--- js/currentLocation.js
+++ js/currentLocation.js
@@ -1,10 +1,60 @@
+
 exports.initMap = function() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
   var map;
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -0, lng: 0},
       zoom: 3
   });
+  directionsDisplay.setMap(map);
+  var onChangeHandler = function() {
+    exports.calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+
+
+  document.getElementById('end').addEventListener('change', onChangeHandler);
+};
+
+function geocodeLatLng(input1) {
+  var output;
+  var done = false;
+  var latlng = input1;
+  var geocoder = new google.maps.Geocoder;
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        console.log(results[1].formatted_address);
+        output = results[1].formatted_address;
+        done = true;
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+  
+  console.log("hey this one" + output);
+  return output;
 }
+
+exports.calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
+
+
+  directionsService.route({
+    origin: geocodeLatLng(userLatLng),
+    destination: document.getElementById('end').value,
+
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+};
 
 exports.locateUser = function() {
     // If the browser supports the Geolocation API
@@ -23,7 +73,7 @@ exports.locateUser = function() {
   // we do this on line 13 above. We pass in a function to call on success, a function to call on error, and some options to tell the geolocation api how we want it to run.
   // on successfully locating the user, geolocationSuccess() gets called automatically, and it is passed the user's position as an argument.
   // on error, geolocationError is called.
-}
+};
 
 exports.geolocationSuccess = function(position) {
 
@@ -33,12 +83,14 @@ exports.geolocationSuccess = function(position) {
   // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
   // But you must use coordinates to use this method.
 
-  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
 
   var myOptions = {
     zoom : 16,
     center : userLatLng,
     mapTypeId : google.maps.MapTypeId.ROADMAP
+
   };
   // Draw the map - you have to use 'getElementById' here.
   var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
@@ -47,10 +99,10 @@ exports.geolocationSuccess = function(position) {
     map: mapObject,
     position: userLatLng
   });
-}
+};
 
 exports.geolocationError = function(positionError)
 {
-  alert(positionError);
+    console.log(positionError);
 
-}
+};
