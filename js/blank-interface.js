3887cfe83255aebebaa8293fce6b3f2b4a08c766
Alex Fallenstedt and Dustin Davis <student@epicodus.com>
2016-03-16 13:27:45 -0700

    move functions to separate js file and fin current location

--- js/blank-interface.js
+++ js/blank-interface.js
@@ -1,13 +1,7 @@
 var currentLocation = require('./../js/currentLocation.js');
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 10
-  });
-}
+
 $(document).ready(function() {
-  google.maps.event.addDomListener(window, 'load', initMap);
-  $('#locateUser').click(currentLocation);
+  google.maps.event.addDomListener(window, 'load', currentLocation.initMap);
+  $('#locateUser').click(currentLocation.locateUser);
 });
--- js/currentLocation.js
+++ js/currentLocation.js
@@ -1,48 +1,56 @@
-exports.currentLocation = function() {
-  //google maps functions
-  //google maps functions
-  function locateUser() {
+exports.initMap = function() {
+  var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -0, lng: 0},
+      zoom: 3
+  });
+}
+
+exports.locateUser = function() {
     // If the browser supports the Geolocation API
     if (navigator.geolocation){
       var positionOptions = {
         enableHighAccuracy: true,
         timeout: 10 * 1000 // 10 seconds
       };
-      navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+      navigator.geolocation.getCurrentPosition(exports.geolocationSuccess, exports.geolocationError, positionOptions);
     }
     else {
       alert("Your browser doesn't support the Geolocation API");
     }
-  }
+
   // this is the success callback from telling the navigator (your browser) to get the current user's position
   // we do this on line 13 above. We pass in a function to call on success, a function to call on error, and some options to tell the geolocation api how we want it to run.
   // on successfully locating the user, geolocationSuccess() gets called automatically, and it is passed the user's position as an argument.
   // on error, geolocationError is called.
+}
 
+exports.geolocationSuccess = function(position) {
 
-  function geolocationSuccess(position) {
-    // here we take the `position` object returned by the geolocation api
-    // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
-    // it 2 arguments: one for latitude, one for longitude.
-    // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
-    // But you must use coordinates to use this method.
-    var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  // here we take the `position` object returned by the geolocation api
+  // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
+  // it 2 arguments: one for latitude, one for longitude.
+  // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
+  // But you must use coordinates to use this method.
 
-    var myOptions = {
-      zoom : 16,
-      center : userLatLng,
-      mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
-    // Draw the map - you have to use 'getElementById' here.
-    var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-    // Place the marker
-    new google.maps.Marker({
-      map: mapObject,
-      position: userLatLng
-    });
-  }
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-  function geolocationError(positionError) {
-    alert(positionError);
-  }
+  var myOptions = {
+    zoom : 16,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+  // Draw the map - you have to use 'getElementById' here.
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  // Place the marker
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+exports.geolocationError = function(positionError)
+{
+  alert(positionError);
+
 }
