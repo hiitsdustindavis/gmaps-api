46bcd3ca12f3d8c68320b02baa2e7cd36ccaad04
Alex Fallenstedt and Dustin Davis <student@epicodus.com>
2016-03-16 10:50:37 -0700

    add initial files

---
+++ js/blank-interface.js
@@ -0,0 +1,12 @@
+// var currentMap = require('./../js/map.js');
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 10
+  });
+}
+$(document).ready(function() {
+  google.maps.event.addDomListener(window, 'load', initMap);
+
+});
